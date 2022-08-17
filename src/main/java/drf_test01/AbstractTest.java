05157a4b04eda05660106bcce4d947a28c644ed5
ruifuding <ruifuding@huanle.com>
2022-08-17 23:22:18 +0800

    TestBranch commit

--- src/main/java/drf_test01/AbstractTest.java
+++ src/main/java/drf_test01/AbstractTest.java
@@ -5,6 +5,7 @@ public abstract class AbstractTest {
         System.out.println("select \"_accountid\" as \"#account_id\", \"_distinctid\" as \"#distinct_id\", \"_eventname\" as \"#event_name\", \"_ip\" as \"#ip\", \"_time\" as \"#time\", 8 as \"#zone_offset\", \"app_plat\" as \"app_plat\", \"ad_channel\" as \"ad_channel\", \"uin\" as \"uin\", \"group_id\" as \"group_id\", \"level\" as \"level\", \"total_pay_money\" as \"total_pay_money\", \"device_os\" as \"device_os\", \"honour\" as \"honour\", \"backflow_channel\" as \"backflow_channel\", \"share_src_ad_channel\" as \"share_src_ad_channel\", \"config_group\" as \"config_group\", 'yxs' as \"app\" from \"yxs\".\"dwd_register_log\" where date ='${day}'");
         System.out.println("second");
         System.out.println("third");
+        System.out.println("TestBranch");
     }
 
 }
